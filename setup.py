8f3934355362c46702557b804700adad6b7d83d5
yunus5603 <syunus838@gmail.com>
2023-12-17 20:12:45 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mohammad Yunus Shaikh",
+    author_email="syunus838@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
---
+++ src/constant/__init__.py
@@ -0,0 +1,4 @@
+AWS_S3_BUCKET_NAME = "sensor-deployment"
+MONGO_DATABASE_NAME = "test"
+MONGO_COLLECTION_NAME = "test1"
+MONGO_DB_URL = "mongodb+srv://Yunus:Shaikh@mlprojectcluster.zrftjm2.mongodb.net/?retryWrites=true&w=majority"
